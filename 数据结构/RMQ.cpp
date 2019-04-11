a2f51c0ba865b7ae194a161ac9551802c8a684e9
Tooc0ld <740231684@qq.com>
2019-04-11 16:14:53 +0800

    abc

--- 数据结构/RMQ.cpp
+++ 数据结构/RMQ.cpp
@@ -4,7 +4,7 @@ I namespace RMQ {
         for(int i = 1; i <= n; i++) dp[i][0] = A[i];
         for(int j = 1; (1<<j) <= n; j++)
             for(int i = 1; i+(1<<j)-1 <= n; i++)
-                dp[i][j] = min(dp[i][j-1], dp[i+(1<<(j-1))][j-1]);
+                dp[i][j] = min(dp[i][j-1], dp[i+(1<<j-1)][j-1]);
     }
     I int RMQ(int L, int R) { //下标从1开始
         int k = Log[R-L+1];
